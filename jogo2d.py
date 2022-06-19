ee773ed7e1b3a879c8f289d7bdab640f140162e0
Vitor Augusto Pereto <vitoraugustopereto@gmail.com>
2022-06-19 20:01:18 -0300

    fundo repetitivo

--- jogo2d.py
+++ jogo2d.py
@@ -2,8 +2,8 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
-x = 600
-y = 800
+x = 1280
+y = 600
 
 screen = pygame.display.set_mode((x, y))
 pygame.display.set_caption('Top Gun')
@@ -19,5 +19,13 @@ while rodando:
             rodando = False
 
     screen.blit(bg, (0, 0))
+
+    rel_x = x % bg.get_rect().width
+    screen.blit(bg, (rel_x - bg.get_rect().width,0))
+    if rel_x < 1200:
+        screen.blit(bg, (rel_x,0))
+
+    #movimento
+    x-=0.6
 
     pygame.display.update()
