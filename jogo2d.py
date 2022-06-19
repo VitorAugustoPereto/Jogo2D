421d32275ee1e73bc0d45f9f8810416012de7fd8
Vitor Augusto Pereto <vitoraugustopereto@gmail.com>
2022-06-19 20:26:51 -0300

    adicionada as imagens das naves

--- jogo2d.py
+++ jogo2d.py
@@ -8,6 +8,24 @@ y = 600
 screen = pygame.display.set_mode((x, y))
 pygame.display.set_caption('Top Gun')
 
+naveInimiga = pygame.image.load("assets/Nave2.PNG.png").convert_alpha()
+naveInimiga = pygame.transform.scale(naveInimiga,(50,50))
+naveInimiga = pygame.transform.rotate(naveInimiga, 90   )
+
+#posição inicial
+pos_naveInimiga_x = 500
+pos_naveInimiga_y = 360
+
+
+navePlayer = pygame.image.load("assets/Nave1.PNG.png").convert_alpha()
+navePlayer = pygame.transform.scale(navePlayer,(50,50))
+navePlayer = pygame.transform.rotate(navePlayer, -90)
+
+#posição inicial
+pos_navePlayer_x = 200
+pos_navePlayer_y = 300
+
+
 bg = pygame.image.load('assets/Fundo.PNG.png').convert_alpha()
 bg = pygame.transform.scale(bg, (x, y))
 
@@ -27,5 +45,9 @@ while rodando:
 
     #movimento
     x-=0.6
+
+    #criada imagens
+    screen.blit(naveInimiga, (pos_naveInimiga_y, pos_naveInimiga_y))
+    screen.blit(navePlayer, (pos_navePlayer_x, pos_navePlayer_y))
 
     pygame.display.update()
